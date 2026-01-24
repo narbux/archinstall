869dd5f6fc4a7d6d648c7943be31de7fff37e422
narbux <mail@marnixenthoven.nl>
2026-01-24 14:09:39 +0100

    Delete source ~/.zshrc

--- post-install-user.sh
+++ post-install-user.sh
@@ -2,10 +2,8 @@
 
 set -euo pipefail
 
-
-message()
-{
-    echo -e "\e[1;31m>> \e[0m$1"
+message() {
+  echo -e "\e[1;31m>> \e[0m$1"
 }
 
 message "** Starting userspace post install script **"
@@ -13,28 +11,28 @@ message "** Starting userspace post install script **"
 # User SSH setting
 message "Downloading ssh keys"
 if [[ ! -d "$HOME"/.ssh ]]; then
-    mkdir $HOME/.ssh
+  mkdir $HOME/.ssh
 fi
 
 if [[ -e /usr/bin/curl ]]; then
-    curl -SsL https://github.com/narbux.keys -o .ssh/authorized_keys
+  curl -SsL https://github.com/narbux.keys -o .ssh/authorized_keys
 else
-    message "ERROR: Could not find curl to download SSH keys"
+  message "ERROR: Could not find curl to download SSH keys"
 fi
 
 # Install PARU AUR helper
 message "Downloading Paru"
-git clone --depth=1 https://aur.archlinux.org/paru 1>/dev/null \
-    && cd paru \
-    && makepkg -si --noconfirm 1>/dev/null \
-    && cd ~ \
-    && rm -rf paru
+git clone --depth=1 https://aur.archlinux.org/paru 1>/dev/null &&
+  cd paru &&
+  makepkg -si --noconfirm 1>/dev/null &&
+  cd ~ &&
+  rm -rf paru
 
 # Install and configure zsh-antidote and zsh
 message "Downloading ZSH-antidote and configuring ZSH"
 paru -S --noconfirm zsh-antidote 1>/dev/null
 
-cat <<'EOF' >> $HOME/.zsh_plugins.txt
+cat <<'EOF' >>$HOME/.zsh_plugins.txt
 mattmc3/ez-compinit
 zsh-users/zsh-completions kind:fpath path=src
 
@@ -44,7 +42,7 @@ zsh-users/zsh-autosuggestions
 zdharma-continuum/fast-syntax-highlighting kind:defer
 EOF
 
-cat <<'EOF' >> $HOME/.zshrc
+cat <<'EOF' >>$HOME/.zshrc
 EDITOR=nvim
 VISUAL=nvim
 
@@ -67,7 +65,6 @@ autoload -Uz promptinit && promptinit && prompt pure
 
 eval "$(zoxide init zsh)"
 EOF
-source $HOME/.zshrc
 
 message "Removing Bash leftover files"
 rm $HOME/.bash_logout $HOME/.bash_profile $HOME/.bashrc
